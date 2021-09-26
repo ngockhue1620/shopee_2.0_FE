d1767a0c7bc371e1de968e05c80d662805c76a8f
Your Name <hoanglong0507000@gmail.co$ git config --global user.email hoanglong0507000@gmail.co>
2021-09-26 17:58:48 +0700

    update

--- src/modules/shopee/pages/Login.js
+++ src/modules/shopee/pages/Login.js
@@ -1,8 +1,14 @@
-import React from 'react'
-import { MainLayout } from '../layouts/MainLayout'
+import React from "react";
+import { MainLayout } from "../layouts/MainLayout";
+import { useShopeeApiClient } from "../hooks/useShopeeApiClient";
+import useAsync from "../../common/hooks/useAsync";
 
 export default function Login() {
-    return (
-        <MainLayout>login page</MainLayout>
-    )
+  const api = useShopeeApiClient();
+  const login = useAsync(async (data) => {
+    const result = await api.login(data);
+    if (result) {
+    }
+  });
+  return <MainLayout>login page</MainLayout>;
 }
